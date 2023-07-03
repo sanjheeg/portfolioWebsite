5bc76b7bc57da6a1acf435de00ec05bb6770b4a1
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-03 14:47:36 -0700

    os

--- set_email.py
+++ set_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,7 +8,7 @@ def send_email(message):
     port = 465
 
     username = "sanjheeg30@gmail.com"
-    password = "xrddlkavgpuedycb"
+    password = os.getenv("PASSWORD")
     receiver = "sanjheeg30@gmail.com"
 
     context = ssl.create_default_context()
