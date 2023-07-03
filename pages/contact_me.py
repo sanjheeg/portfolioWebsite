3b3df1e6521363fd6abe6e25adc3982aae33d8c9
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-03 14:20:01 -0700

    added topics

--- pages/contact_me.py
+++ pages/contact_me.py
@@ -3,13 +3,17 @@ from set_email import send_email
 
 sl.header("contact me!")
 
+options = ["experience", "volunteering", "college", "other"]
+
 with sl.form(key="email_form"):
     email = sl.text_input("your email address")
+    topic = sl.selectbox("what topic do you want to discuss?", options)
     message = sl.text_area("your message")
     send_message = f"""\
 Subject: new email from portfolio
 
 from: {email}
+topic: {topic}
 {message}
 """
     button = sl.form_submit_button("submit")
